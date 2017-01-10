755745b27327e87f47a19a61f0bfe73aedfdd224
wufan <wufan-2304@foxmail.com>
2017-01-10 15:06:05 +0800

    initial

---
+++ pages/home/index.js
@@ -0,0 +1,30 @@
+// pages/home/index.js
+Page({
+  data:{
+    text: 'init data',
+    person1:{
+      name:"wufan",
+      content:"this is content",
+      l_h_number:"100",
+      time:"2h",
+      distance:"150m",
+      comments_number:"20"
+
+    }
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
